cea4265b7875bfa9b658bfcc2414f3141b3facdd
anneloviseramstad <160476303+anneloviseramstad@users.noreply.github.com>
2025-04-04 11:52:13 +0200

    Cleaned up and deleted auto generated files from installation of vite and tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./**/*.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      fontWeight: {
+        thin: 100,
+        light: 300,
+      },
+    },
   },
   plugins: [],
 };
